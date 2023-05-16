d867042838977e2bc0905035c734f241c903b8c0
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-16 16:02:35 +0200

    Bonus 2: added a different classList for every liElement != number

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ for(let i = 1; i <= 100; i++){
 
     if(i % 3 === 0 && i % 5 === 0){
        liElement.innerHTML = `FizzBuzz`;
+       liElement.classList.add('red');
 
     } else if(i % 5 === 0){
         liElement.innerHTML = `Buzz`;
+        liElement.classList.add('green');
 
     }else if(i % 3 === 0){
         liElement.innerHTML = `Fizz`
+        liElement.classList.add('blue');
     }
     else{
         liElement.innerHTML = i;
