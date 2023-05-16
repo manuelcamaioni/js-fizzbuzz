d5d788f72312f8a428fcba486f8be647a0654f90
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-16 16:17:54 +0200

    added comments

--- script.js
+++ script.js
@@ -3,24 +3,27 @@ const ulElement = document.querySelector('ul');
 
 for(let i = 1; i <= 100; i++){
     const liElement = document.createElement('li'); // creo una variabile con tag <li> </li> vuota per ogni iterazione
+    
     // ? document.querySelector('li').style.listStyleType = none; non funziona
 
+    // * popolo la variabile in base al risultato della condizione!
+
     if(i % 3 === 0 && i % 5 === 0){
-       liElement.innerHTML = `FizzBuzz`;  // se rispetta la condizione, il contenuto diventa questa stringa
-       liElement.classList.add('red');    // aggiungo una classe all'elemento html, nel caso rispettasse questa condizione
+       liElement.innerHTML = `FizzBuzz`;    // se rispetta la condizione, il contenuto diventa questa stringa
+       liElement.classList.add('red');      // aggiungo una classe all'elemento html, nel caso rispettasse questa condizione
 
     } else if(i % 5 === 0){
-        liElement.innerHTML = `Buzz`; // se ripetta questa condizione, diventa questa
-        liElement.classList.add('green'); // aggiungo una classe all'elemento html, nel caso rispettasse questa condizione
+        liElement.innerHTML = `Buzz`;       // se ripetta questa condizione, diventa questa
+        liElement.classList.add('green');   // aggiungo una classe all'elemento html, nel caso rispettasse questa condizione
 
     }else if(i % 3 === 0){
-        liElement.innerHTML = `Fizz` // se ripetta questa condizione, diventa questa
-        liElement.classList.add('blue'); // aggiungo una classe all'elemento html, nel caso rispettasse questa condizione
+        liElement.innerHTML = `Fizz`        // se ripetta questa condizione, diventa questa
+        liElement.classList.add('blue');    // aggiungo una classe all'elemento html, nel caso rispettasse questa condizione
     }
     else{
-        liElement.innerHTML = i; // altrimenti prende il valore di i a n iterazione
+        liElement.innerHTML = i;            // altrimenti prende il valore di i a n iterazione
     }
     
-    ulElement.append(liElement); // stampo il valore al termin della condizione nella pagina html, in una variabile js con all'interno un elemento html, in questo caso <ul> </ul>
+    ulElement.append(liElement);            // stampo il valore al termine della condizione nella pagina html, in una variabile js con                                         all'interno un elemento html, in questo caso <ul> </ul>
 }
 
