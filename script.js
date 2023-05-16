dea5e0d31d03c69df82c9f226e16124c5c54bb8f
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-16 15:45:38 +0200

    test

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
 const ulElement = document.querySelector('ul');
 const liElement = document.createElement('li');
 
+for(let i = 1; i <= 100; i++){
+    // if(i % 3 === 0 && i % 5 === 0){
+    //     liElement.innerHTML = `FizzBuzz`;
 
-// for(let i = 1; i <= 100; i++){
-//     if(i % 3 === 0 && i % 5 === 0){
-//         console.log(`FizzBuzz (${i})`);
+    // } else if(i % 5 === 0){
+    //     liElement.innerHTML = `Buzz`;
 
-//     } else if(i % 5 === 0){
-//         console.log(`Buzz (${i})`);
-
-//     }else if(i % 3 === 0){
-//         console.log(`Fizz (${i})`);
-//     }
-//     else{
-//         console.log(i);
-//     }
-    
-// }
-
-let i = 1;
-
-liElement.innerHTML = parseInt(i);
-console.log(liElement);
+    // }else if(i % 3 === 0){
+    //     liElement.innerHTML = `Fizz`
+    // }
+    // else{
+    //     liElement.innerHTML = i;
+    // }
+    liElement.innerHTML = i;
+    ulElement.append(liElement);
+    console.log(liElement);
+}
