05dba7905f3c401bd60d19ba1032287539dcbffa
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-16 15:29:21 +0200

    test

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-for(let i = 1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-        console.log(`FizzBuzz (${i})`);
+const ulElement = document.querySelector('ul');
+const liElement = document.createElement('li');
 
-    } else if(i % 5 === 0){
-        console.log(`Buzz (${i})`);
 
-    }else if(i % 3 === 0){
-        console.log(`Fizz (${i})`);
-    }
-    else{
-        console.log(i);
-    }
+// for(let i = 1; i <= 100; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(`FizzBuzz (${i})`);
+
+//     } else if(i % 5 === 0){
+//         console.log(`Buzz (${i})`);
+
+//     }else if(i % 3 === 0){
+//         console.log(`Fizz (${i})`);
+//     }
+//     else{
+//         console.log(i);
+//     }
     
-}
+// }
+
+let i = 1;
+
+liElement.innerHTML = parseInt(i);
+console.log(liElement);
