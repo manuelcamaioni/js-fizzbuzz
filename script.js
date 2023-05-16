4997c892b4ea0dc8abfa8f8074a25b92364b79f4
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-16 15:59:09 +0200

    Bonus 1: created li element in js and stamped in index.html 100 times + added classes in css

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 const ulElement = document.querySelector('ul');
-const liElement = document.createElement('li');
+
 
 for(let i = 1; i <= 100; i++){
-    // if(i % 3 === 0 && i % 5 === 0){
-    //     liElement.innerHTML = `FizzBuzz`;
-
-    // } else if(i % 5 === 0){
-    //     liElement.innerHTML = `Buzz`;
-
-    // }else if(i % 3 === 0){
-    //     liElement.innerHTML = `Fizz`
-    // }
-    // else{
-    //     liElement.innerHTML = i;
-    // }
+    const liElement = document.createElement('li');
     liElement.innerHTML = i;
+
+    if(i % 3 === 0 && i % 5 === 0){
+       liElement.innerHTML = `FizzBuzz`;
+
+    } else if(i % 5 === 0){
+        liElement.innerHTML = `Buzz`;
+
+    }else if(i % 3 === 0){
+        liElement.innerHTML = `Fizz`
+    }
+    else{
+        liElement.innerHTML = i;
+    }
+    
     ulElement.append(liElement);
-    console.log(liElement);
 }
